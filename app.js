9d8c30a06aa50089b80d99cfb1973a79fb6f5e48
unknown <djkolesa@gmail.com>
2020-07-10 23:46:28 -0600

    David Kolesar has added ES6 promise example

--- app.js
+++ app.js
@@ -4,5 +4,22 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Express Server -- istening on port 3000!');
+  console.log('Executing promise example:');
+
+	let myFirstPromise = new Promise((resolve, reject) => {
+	  // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+	  // In this example, we use setTimeout(...) to simulate async code. 
+	  // In reality, you will probably be using something like XHR or an HTML5 API.
+	  setTimeout( function() {
+		resolve("Success")  // success
+	  }, 2000) 
+	}) 
+
+	myFirstPromise.then((successMessage) => {
+	  // successMessage is whatever we passed in the resolve(...) function above.
+	  // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+	  console.log("Promise Kept! " + successMessage) 
+	});
+	
 });
